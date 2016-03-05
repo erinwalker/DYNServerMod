58b2f3d2337ec2ad4c4a727e33366951ec797abd
DomAmato <dominic.a.amato@gmail.com>
2016-03-04 21:35:38 -0600

    change update script

--- update.sh
+++ update.sh
@@ -1,51 +1,25 @@
-#change to the Achievement submodule
-cd src/main/java/com/dyn/achievements
-#update the Achievement submodule
-git submodule update
-#back to the home folder
-cd ../../../../../..
+git submodule foreach git pull origin submodule
 
-#change to the Login submodule
-cd src/main/java/com/dyn/login
-#update the Login submodule
-git submodule update
-#back to the home folder
-cd ../../../../../..
+# Stage the changes
+git add src/main/java/com/dyn/achievements
 
-#change to the Server submodule
-cd src/main/java/com/dyn/server
-#update the Server submodule
-git submodule update
-#back to the home folder
-cd ../../../../../..
+# Stage the changes
+git add src/main/java/com/dyn/login
 
-#change to the Teacher submodule
-cd src/main/java/com/dyn/instructor
-#update the Teacher submodule
-git submodule update
-#back to the home folder
-cd ../../../../../..
+# Stage the changes
+git add src/main/java/com/dyn/server
 
-#change to the Item submodule
-cd src/main/java/com/dyn/item
-#update the Item submodule
-git submodule update
-#back to the home folder
-cd ../../../../../..
+# Stage the changes
+git add src/main/java/com/dyn/instructor
 
-#change to the assets submodule
-cd src/main/java/assets
-#update the Assets submodule
-git submodule update
-#back to the home folder
-cd ../../../..
+# Stage the changes
+git add src/main/java/com/dyn/item
 
-#change to the Rabbit submodule
-cd src/main/java/com/rabbit
-#update the Rabit submodule
-git submodule update
-#back to the home folder
-cd ../../../../..
+# Stage the changes
+git add src/main/java/assets
+
+# Stage the changes
+git add src/main/java/com/rabbit
 
 # Commit the changes
 git commit -m "Updating Submodules"
